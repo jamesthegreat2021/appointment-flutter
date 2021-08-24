e43e827e2f285b715fc869a6ca08b8859af3bc99
jjames2021 <jamesmemba2000@gmail.com>
2021-08-24 15:36:41 +0300

    first commit

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,21 +1,23 @@
-
+import 'package:auntie_rafiki/pages/Home_page.dart';
 import 'package:auntie_rafiki/pages/boom_color_changer_page.dart';
+
 import 'package:flutter/material.dart';
 
-import 'pages/my_home_page.dart';
-
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-      
         primarySwatch: Colors.orange,
       ),
-      home: BoomColorChanger(),
+      home: HomePage(),
+      routes: {
+        "boom_color_changer": (context) {
+          return BoomColorChanger();
+        }
+      },
     );
   }
 }
---
+++ lib/pages/Home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PT PROJECT"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("UNIVERSITYOF DAR ES SALAAM"),
+            Image.asset("assets/icons/icon.jpg"),
+          ],
+        ),
+      ),
+      drawer: Container(
+        color: Colors.orange,
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(accountName: Text("james"), accountEmail: Text("jamesmemba2000@gmail.com"), currentAccountPicture: CircleAvatar(backgroundImage: AssetImage("assets/images/profile.jpg")),),
+            /*AppBar(
+              title: Text("INDUSTRIAL PROJECT"),
+            ),*/
+            ListTile(
+              /* leading: CircleAvatar(
+              backgroundColor: Colors.white,
+              ),*/
+              
+              leading: Icon(Icons.ac_unit_sharp),
+              title: Text("boom color changer"),
+              onTap: () {
+                Navigator.of(context).pushNamed("boom_color_changer");
+              },
+            ),
+            ListTile(
+                /* leading: CircleAvatar(
+              backgroundColor: Colors.white,
+              ),*/
+                leading: Icon(Icons.ac_unit_sharp),
+                title: Text("boom color changer")),
+          ],
+        ),
+      ),
+    );
+  }
+}
